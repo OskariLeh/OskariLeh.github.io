5a695b4e6c5926f3ba5ecced7df54d20af0e5aa5
Oskari Lehtonen <oskari.m.lehtonen@student.lut.fi>
2023-01-06 14:38:22 +0200

    Contact page and a bit better tic tac toe

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,10 +3,13 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const navItems = document.querySelectorAll(".nav-item");
 
+
+
 // Initial state of menu
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
+
 
 function toggleMenu() {
     if(!showMenu){
@@ -22,5 +25,50 @@ function toggleMenu() {
         menuNav.classList.remove("show"); 
         navItems.forEach(item => item.classList.remove("show"));
         showMenu = false;
+    }
+}
+
+
+//Handle the tic tac toe game
+const clearBtn = document.querySelector(".clear-btn");
+const boxes = document.querySelectorAll(".box");
+const turnIndicator = document.querySelector(".turn-indicator");
+
+let counter = 0;
+
+boxes.forEach(item => item.addEventListener("click", () => { boxClicked(item); }));
+clearBtn.addEventListener("click", clear);
+turnIndicator.classList.add("circles");
+
+function boxClicked(box) {
+    if(!box.classList.contains("circle") && !box.classList.contains("cross") && counter % 2 == 0){
+        box.classList.add("circle");
+        counter += 1;
+    }else if (!box.classList.contains("circle") && !box.classList.contains("cross") && counter % 2 != 0){
+        box.classList.add("cross");
+        counter += 1;
+    }
+    if (counter % 2 == 0) {
+        turnIndicator.classList.remove("crosses");
+        turnIndicator.classList.add("circles");
+    } else {
+        turnIndicator.classList.remove("circles");
+        turnIndicator.classList.add("crosses");
+    }
+
+}
+
+function clear(){
+    for (let i = 0; i < boxes.length; i++) {
+        if (boxes[i].classList.contains("cross")) {
+            boxes[i].classList.remove("cross")
+        }else if (boxes[i].classList.contains("circle")) {
+            boxes[i].classList.remove("circle")
+        }
+        counter = 0;
+        if (turnIndicator.classList.contains("crosses")){
+            turnIndicator.classList.remove("crosses");
+            turnIndicator.classList.add("circles");
+        }
     }
 }
